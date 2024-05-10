f5f7df45c8f22bfc0023a8a197584774d8e9cfd1
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-05-10 18:54:06 +0900

    VCを切り替えるとタブが切り替わる

--- rakuten_ranking_app/Views/RankingViewController.swift
+++ rakuten_ranking_app/Views/RankingViewController.swift
@@ -7,59 +7,69 @@
 
 import UIKit
 
-//delegateの設定のためにprotocolを設定
+
+//選択されたタブに応じて表示するビューコントローラを変更するためのデリゲート
 protocol RankingViewControllerDelegate: AnyObject {
-    func switchToViewController(at index: Int)
+    func changeViewControllerByTab(at index: Int)
 }
 
-class RankingViewController: UIViewController, UIPageViewControllerDelegate {
-
+class RankingViewController: UIViewController, UIPageViewControllerDelegate, RankingPageViewControllerDelegate {
+    
     @IBOutlet weak var switchedView: UIView!
     //segmentedControlの紐付け
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    private var rankingPageViewController: RankingPageViewController!
+    private var rankingPageViewController: RankingPageViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        //わかりやすい様に切り分ける
+        setupPageViewController()
+        setupSegmentedControl()
+    }
+    
+    private func setupPageViewController() {
+        //ストーリーボードからRankingPageViewControllerをインスタンス化
         guard let rankingPageViewController = storyboard?.instantiateViewController(withIdentifier: "rankingPageViewController") as? RankingPageViewController else {
-               print("StoryboardからRankingPageViewControllerのインスタンス化に失敗しました")
-               return
-           }
-  
+            print("StoryboardからRankingPageViewControllerのインスタンス化に失敗しました")
+            return
+        }
+        
         addChild(rankingPageViewController)
+        //switchedViewに追加
         if let switchedView = switchedView {
             switchedView.addSubview(rankingPageViewController.view)
             rankingPageViewController.view.frame = switchedView.bounds
             rankingPageViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            rankingPageViewController.didMove(toParent: self)
+            self.rankingPageViewController = rankingPageViewController
+            //デリゲート
+            rankingPageViewController.customDelegate = self
+        } else {
+            print("switchedViewがStoryboardで正しく接続されていません。")
         }
-        rankingPageViewController.didMove(toParent: self)
-        self.rankingPageViewController = rankingPageViewController
-        
-        //delegateの設定
-        rankingPageViewController.delegate = self
-        
-        // segmentedControlの設定
+    }
+    //セグメントコントロールを設定
+    private func setupSegmentedControl() {
         if let segmentedControl = segmentedControl {
             segmentedControl.removeAllSegments()
             segmentedControl.insertSegment(withTitle: "総合", at: 0, animated: false)
             segmentedControl.insertSegment(withTitle: "男性", at: 1, animated: false)
             segmentedControl.insertSegment(withTitle: "女性", at: 2, animated: false)
             segmentedControl.selectedSegmentIndex = 0
-            //アクションを割り当てる　セグメントの選択が変更された時 → segmentChanged(_:)メソッドを呼び出す
             segmentedControl.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
         } else {
             print("segmentedControlがStoryboardで正しく接続されていません。")
         }
     }
     
-    
-    //segmentChanged(_:)メソッド
+    //タップされたときに呼び出されるアクション
     @objc private func segmentChanged(_ sender: UISegmentedControl) {
-        //現在選択されているセグメントのインデックスを取得
-        rankingPageViewController.switchToViewController(at: sender.selectedSegmentIndex)
+        rankingPageViewController?.switchToViewController(at: sender.selectedSegmentIndex)
+    }
+    
+    //ビューコントローラがスワイプで変更されたときにセグメントの選択を更新
+    func changeTabByViewController(at index: Int) {
+        segmentedControl?.selectedSegmentIndex = index
     }
 }
-
-
