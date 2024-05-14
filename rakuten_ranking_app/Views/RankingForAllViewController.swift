e2c8b05a75d13e05a58f0692413bd6f7939d7afd
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-05-14 12:33:10 +0900

    Observer周りを追加するもどうすればいいのかわからなくなってきた。

--- rakuten_ranking_app/Views/RankingForAllViewController.swift
+++ rakuten_ranking_app/Views/RankingForAllViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 
 class RankingForAllViewController: UIViewController {
+    
+    var model: RankingModel! {
+      didSet {
+        registerModel()
+      }
+    }
+    
+    deinit {
+           model.notificationCenter.removeObserver(self)
+    }
+
+    private func registerModel() {
+      _ = model.notificationCenter
+               .addObserver(forName: .init(rawValue: "ranking"),
+                            object: nil, queue: nil) { [weak self] notification in
+                               if let ranking = notification.userInfo?["ranking"] as? Ranking {
+                                   self?.printData(ranking: ranking)
+                               }
+      }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        printData(ranking: model.ranking)
     }
     
-
+    private func printData(ranking: Ranking) {
+        for itemElement in ranking.items {
+              let item = itemElement.item
+              print("商品名: \(item.itemName)")
+          }
+    }
 }
