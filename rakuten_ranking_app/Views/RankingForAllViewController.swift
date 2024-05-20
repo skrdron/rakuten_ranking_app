e876b7e68fa64c86bd4431316f9d27a05ad180a5
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-05-20 11:09:52 +0900

    Top30の商品取得

--- rakuten_ranking_app/Views/RankingForAllViewController.swift
+++ rakuten_ranking_app/Views/RankingForAllViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class RankingForAllViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var model: RankingModel! {
       didSet {
         registerModel()
       }
     }
+    
+    private var items: [ItemElement] = []
     
     deinit {
       model.notificationCenter.removeObserver(self)
@@ -25,6 +29,8 @@ class RankingForAllViewController: UIViewController {
                             object: nil, queue: nil) { notification in
                                if let ranking = notification.userInfo?[NotificationConst.rankingNotificationName] as? Ranking {
                                    ranking.printData()
+                                   self.items = ranking.items
+                                   self.tableView.reloadData()
                                }
       }
     }
@@ -32,7 +38,43 @@ class RankingForAllViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         model = RankingModel(sex: .all, apiClient: DefaultAPIClient.shared)
         model.requestRanking()
     }
+}
+
+
+extension RankingForAllViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RankingCell", for: indexPath) as! RankingTableViewCell
+        let item = items[indexPath.row].item
+        tableView.rowHeight = 110
+        
+        cell.rankingLabel.text = "\(item.rank)"
+        cell.productNameLabel.text = item.itemName
+        cell.priceLabel.text = "¥\(item.itemPrice)"
+        
+        if let urlString = item.mediumImageUrls.first?.imageURL, let url = URL(string: urlString) {
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        cell.productImageView.image = UIImage(data: data)
+                    }
+                }
+            }
+        }
+        
+        return cell
+    }
+}
+
+extension RankingForAllViewController: UITableViewDelegate {
+
 }
---
+++ rakuten_ranking_app/Views/RankingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RankingTableViewCell.swift
+//  rakuten_ranking_app
+//
+//  Created by 櫻田龍之助 on 2024/05/17.
+//
+
+import UIKit
+
+class RankingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var rankingLabel: UILabel!
+}
