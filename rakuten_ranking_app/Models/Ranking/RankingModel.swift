667022867039f3c8f712d351fe9ff8169582dc6e
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-05-21 15:04:29 +0900

    UserInfoの共通化

--- rakuten_ranking_app/Models/Ranking/RankingModel.swift
+++ rakuten_ranking_app/Models/Ranking/RankingModel.swift
@@ -18,8 +18,8 @@ class RankingModel {
                 name: .init(rawValue: NotificationConst.rankingNotificationName),
                 object: nil,
                 userInfo: [
-                    NotificationConst.rankingNotificationName: ranking!,
-                    "sexType": sex
+                    NotificationConst.UserInfoKeys.ranking: ranking!,
+                    NotificationConst.UserInfoKeys.sexType: sex
                 ]
             )
         }
