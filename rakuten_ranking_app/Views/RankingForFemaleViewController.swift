1c35f49feb33b193d21193bf93b234d755b5c85c
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-05-24 15:08:46 +0900

    Revert "searchモデルの再コミット"
    This reverts commit 1bb30eeebcf20b581e73eb11b4fd0eff03a53b2a.

--- rakuten_ranking_app/Views/RankingForFemaleViewController.swift
+++ rakuten_ranking_app/Views/RankingForFemaleViewController.swift
@@ -16,7 +16,7 @@ class RankingForFemaleViewController: UIViewController {
       }
     }
     
-    private var items: [RankingItemElement] = []
+    private var items: [ItemElement] = []
     
     deinit {
       model.notificationCenter.removeObserver(self)
